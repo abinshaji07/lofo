8ad2773e98ffcbc66e90d77e4e9fb192dfacc029
abinshaji07 <abinshajiphilip2023@cs.ajce.in>
2022-07-27 22:58:16 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 // @dart=2.9
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flower_selling_app/firebase_options.dart';
+import 'package:flower_selling_app/home/screens/buy/location.dart';
 import 'package:flutter/material.dart';
 import 'my_app.dart';
+
+import 'package:flutter/widgets.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
-  runApp(const MyApp());
+  // runApp( MaterialApp(
+  //   home: CardPicture(),
+  // ),
+  // );
+   runApp(const MyApp());
 }
